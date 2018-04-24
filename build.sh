75f8a23c55f7d2020b4805434dd24a474ce569cc
Michael Gran <spk121@yahoo.com>
2018-04-24 16:14:44 -0700

    MinGW32 fixes

--- build.sh
+++ build.sh
@@ -31,13 +31,22 @@ if ! test -f libffi-3.2.1.tar; then
     wget ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz
     gunzip libffi-3.2.1.tar.gz
 fi
-if ! test -f guile-2.2.3.tar; then
-    wget https://mirrors.ocf.berkeley.edu/gnu/guile/guile-2.2.3.tar.lz
-    lzip -d guile-2.2.3.tar.lz
-fi
 if ! test -f readline-7.0.tar; then
     wget https://mirrors.ocf.berkeley.edu/gnu/readline/readline-7.0.tar.gz
     gunzip readline-7.0.tar.gz
+fi
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    if ! test -f guile.tar; then
+	wget http://git.savannah.gnu.org/cgit/guile.git/snapshot/guile-925485c27615ad20884b96fd6cff6d3dc08013de.tar.gz
+	gunzip guile-925485c27615ad20884b96fd6cff6d3dc08013de.tar.gz
+	mv guile-925485c27615ad20884b96fd6cff6d3dc08013de.tar guile.tar
+    fi
+else
+    if ! test -f guile.tar; then
+	wget https://mirrors.ocf.berkeley.edu/gnu/guile/guile-2.2.3.tar.lz
+	lzip -d guile-2.2.3.tar.lz
+	mv guile-2.2.3.tar guile.tar
+    fi    
 fi
 if ! test -f libvorbis-1.3.6.tar; then
     wget https://ftp.osuosl.org/pub/xiph/releases/vorbis/libvorbis-1.3.6.tar.xz
@@ -57,8 +66,8 @@ fi
 tar xf libunistring-0.9.9.tar
 tar xf gmp-6.1.2.tar
 tar xf libffi-3.2.1.tar
-tar xf guile-2.2.3.tar
 tar xf readline-7.0.tar
+tar xf guile.tar
 tar xf libvorbis-1.3.6.tar
 unzip -o burro-master.zip
 
@@ -109,12 +118,17 @@ if ! test -f _libffi_complete; then
     make install
     cd $PREFIX
     cp $PREFIX/lib/libffi-3.2.1/include/ffi.h $PREFIX/include/ffi.h
+    cp $PREFIX/lib/libffi-3.2.1/include/ffitarget.h $PREFIX/include/ffitarget.h
     cp $PREFIX/lib64/libffi.a $PREFIX/lib/libffi.a
     echo > _libffi_complete
 fi
 
 if ! test -f _readline_complete; then
+    cp readline-7.0/colors.c readline-7.0/colors.c.orig
+    cp readline-7.0/histfile.c readline-7.0/histfile.c.orig
+    patch -Np0 < readline-7.0-mingw.patch
     cd readline-7.0
+    
     ./configure CC=$CC CFLAGS="-g -O1" --disable-shared --enable-static --prefix=$PREFIX
     make
     make install
@@ -122,17 +136,13 @@ if ! test -f _readline_complete; then
     echo > _readline_complete
 fi
 
-if ! test -f _libvorbis_complete; then
-    cd libvorbis-1.3.6
-    ./configure CC=$CC CFLAGS="-g -O1" --disable-shared --enable-static --prefix=$PREFIX
-    make
-    make install
-    cd $PREFIX
-    echo > _libvorbis_complete
-fi
-
-if ! test -f 0_guile_complete; then
-    cd guile-2.2.3
+if ! test -f _guile_complete; then
+    if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+	cd guile-925485c27615ad20884b96fd6cff6d3dc08013de
+	autoreconf -vif
+    else
+	cd guile-2.2.3
+    fi
     ./configure CC=$CC \
 		CFLAGS="-I$PREFIX/include -I$PREFIX/include/readline -g -O1 -UHAVE_CLOCK_GETTIME -UFFI_CLOSURES" \
 		LDFLAGS= \
@@ -147,11 +157,22 @@ if ! test -f 0_guile_complete; then
     cp config.h.tmp config.h
     make install
     cd $PREFIX
-    echo > 0_guile_complete
+    echo > _guile_complete
+fi
+
+
+if ! test -f _libvorbis_complete; then
+    cd libvorbis-1.3.6
+    ./configure CC=$CC CFLAGS="-g -O1" --disable-shared --enable-static --prefix=$PREFIX
+    make
+    make install
+    cd $PREFIX
+    echo > _libvorbis_complete
 fi
 
 if ! test -f _burro_complete; then
     cd burro-master
+    autoreconf -vif
     ./configure CC=$CC CFLAGS="-g -O1" \
 		LIBS="$PREFIX/lib/libguile.a $PREFIX/lib/gc.a $PREFIX/lib/libffi.a $PREFIX/lib/libgmp.a $PREFIX/lib/libltdl.a $PREFIX/lib/libunistring.a $PREFIX/lib/libreadline.a $PREFIX/lib/libvorbis.a -ldl -lcrypt "	\
 		GUILE_CFLAGS=-I$PREFIX/include/guile/2.2 \
